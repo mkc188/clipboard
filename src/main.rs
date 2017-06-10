708d5f4650ad6dee1c0cdfdc186e60f419714eda
Mok Ka Chun <mkc188@users.noreply.github.com>
2017-06-10 14:14:20 +0800

    Use diesel in iron

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,30 @@
 extern crate iron;
+extern crate clipboard;
+extern crate diesel;
+
+use clipboard::*;
+use self::models::*;
+use diesel::prelude::*;
 
 use iron::prelude::*;
 use iron::status;
 
 fn main() {
+    use self::schema::tunnels::dsl::*;
+
     fn hello_world(_: &mut Request) -> IronResult<Response> {
+        let connection = establish_connection();
+        let results = tunnels.filter(created_time.eq(0))
+            .limit(5)
+            .load::<Tunnel>(&connection)
+            .expect("Error loading tunnels");
+    
+        println!("Displaying {} tunnels", results.len());
+        for tunnel in results {
+            println!("{}", tunnel.mobile_id);
+            println!("-----------\n");
+            println!("{}", tunnel.computer_id);
+        }
         Ok(Response::with((status::Ok, "Hello World!")))
     }
 
