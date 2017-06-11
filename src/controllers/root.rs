6593eb6cc098b4f6f99c688dbc34b9bd72566177
Mok Ka Chun <mkc188@users.noreply.github.com>
2017-06-11 23:36:26 +0800

    Temp use database model in root controller

--- src/controllers/root.rs
+++ src/controllers/root.rs
@@ -2,9 +2,15 @@ use iron::prelude::*;
 use iron::status;
 
 use views;
+use database;
 use models::tunnel::Tunnel;
 
 pub fn handler(req: &mut Request) -> IronResult<Response> {
+    use diesel::prelude::*;
+    use models::schema::tunnels::dsl::*;
+
+    let results = tunnels.filter(created_time.eq(0)).limit(5).load::<Tunnel>(&*database::connection().get().unwrap()).expect("Error loading tunnels");
+
     let resp = Response::with((status::Ok, views::shared::root().unwrap()));
     Ok(resp)
 }
