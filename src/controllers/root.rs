098318bb29ad507f8fd3cb783c66e2871faaf9ba
Mok Ka Chun <mkc188@users.noreply.github.com>
2017-06-11 23:46:55 +0800

    Print something

--- src/controllers/root.rs
+++ src/controllers/root.rs
@@ -9,7 +9,14 @@ pub fn handler(req: &mut Request) -> IronResult<Response> {
     use diesel::prelude::*;
     use models::schema::tunnels::dsl::*;
 
-    let results = tunnels.filter(created_time.eq(0)).limit(5).load::<Tunnel>(&*database::connection().get().unwrap()).expect("Error loading tunnels");
+    // TODO: move to model
+    let results = tunnels.filter(created_time.eq(0)).limit(5).get_result::<Tunnel>(&*database::connection().get().unwrap());
+    println!("Displaying {} tunnels", 0);
+    for tunnel in results {
+        println!("{}", tunnel.mobile_id);
+        println!("-----------\n");
+        println!("{}", tunnel.computer_id);
+    }
 
     let resp = Response::with((status::Ok, views::shared::root().unwrap()));
     Ok(resp)
